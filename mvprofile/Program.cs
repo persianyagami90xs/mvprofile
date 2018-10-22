8d61c2ffe5a5bbb47aa218e5d56e805f3a8f485e
Christopher Lansing <c.lansing@gmail.com>
2018-10-21 22:11:51 -0400

    Create initial "test" app
    A lot of experimenting in here, but the applicatiion is finally
    able to load a registry file that may not already be loaded, look
    through it and print out the changes that it thinks it needs to
    make . Should be able to swap out the console logs for real
    changes at this point.

--- mvprofile/Program.cs
+++ mvprofile/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace mvprofile
 {
@@ -10,6 +13,73 @@ namespace mvprofile
     {
         static void Main(string[] args)
         {
+            string oldName = "Christopher Lansing";
+            string newName = "clansing";
+            string oldDir = $"Users\\{oldName}";
+            string newDir = $"Users\\{newName}";
+
+            if (Privileges.EnablePrivileges("SeRestorePrivilege"))
+            {
+                int loadCode = RegistryUtils.RegLoadKey(RegistryUtils.HKEY_USERS, oldName, @"C:\Users\clansing\clansing.hiv");
+
+                Func<RegistryKey, bool> filter = (key) =>
+                    key.ToString().IndexOf(oldDir, StringComparison.OrdinalIgnoreCase) != -1 ||
+                    key.GetValueNames().Any((name) =>
+                        name.IndexOf(oldDir, StringComparison.OrdinalIgnoreCase) != -1 ||
+                        key.GetValueKind(name) == RegistryValueKind.String && ((string)key.GetValue(name)).IndexOf(oldDir, StringComparison.OrdinalIgnoreCase) != -1 ||
+                        key.GetValueKind(name) == RegistryValueKind.MultiString && ((string[])key.GetValue(name)).Any((value) => value.IndexOf(oldDir, StringComparison.OrdinalIgnoreCase) != -1)
+                    );
+
+                RegistryUtils.IterateKeys(Registry.Users.OpenSubKey(oldName), filter, (key) => UpdateKey(key, oldDir, newDir));
+            }
+            else
+            {
+                Console.Error.WriteLine("Load key privilege not granted.");
+            }
+        }
+
+        static private void UpdateKey(RegistryKey key, string oldDir, string newDir)
+        {
+            string[] names = key.GetValueNames();
+            foreach (string name in names)
+            {
+                if (key.GetValueKind(name) == RegistryValueKind.MultiString)
+                {
+                    string[] oldValues = (string[])key.GetValue(name);
+                    string[] newValues = (string[])oldValues.Select(value => Regex.Replace(value, Regex.Escape(oldDir), newDir, RegexOptions.IgnoreCase));
+
+                    if (!newValues.SequenceEqual(oldValues))
+                    {
+                        Console.Out.WriteLine(String.Join(":", newValues));
+                    }
+                }
+
+                if (key.GetValueKind(name) == RegistryValueKind.String)
+                {
+                    string oldValue = (string)key.GetValue(name);
+                    string newValue = Regex.Replace(oldValue, Regex.Escape(oldDir), newDir, RegexOptions.IgnoreCase);
+
+                    if (oldValue != newValue)
+                    {
+
+                        Console.Out.WriteLine(newValue);
+                    }
+                }
+
+                string newValueName = Regex.Replace(name, Regex.Escape(oldDir), newDir, RegexOptions.IgnoreCase);
+
+                if (newValueName != name)
+                {
+                    Console.Out.WriteLine($"Rename: {newValueName}");
+                }
+            }
+
+            string newKeyName = Regex.Replace(key.Name, Regex.Escape(oldDir), newDir, RegexOptions.IgnoreCase);
+
+            if (key.Name != newKeyName)
+            {
+                Console.Out.WriteLine($"Update key: {newKeyName}");
+            }
         }
     }
 }
---
+++ mvprofile/RegistryUtils.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvprofile
+{
+    class RegistryUtils
+    {
+        public static void IterateKeys(RegistryKey root, Func<RegistryKey, bool> filter, Action<RegistryKey> processor)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Parallel.ForEach(root.GetSubKeyNames(), keyname =>
+            {
+                try
+                {
+                    using (RegistryKey key = root.OpenSubKey(keyname, true))
+                    {
+                        IterateKeys(key, filter, processor);
+
+                        if (filter(key))
+                        {
+                            processor(key);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+            });
+        }
+
+        public static void RenameValue(RegistryKey key, string oldName, string newName)
+        {
+            key.SetValue(newName, key.GetValue(oldName), key.GetValueKind(oldName));
+            key.DeleteValue(oldName);
+        }
+
+        public static void RenameSubKey(RegistryKey root, string oldName, string newName)
+        {
+            using (RegistryKey subKey = root.OpenSubKey(oldName))
+            {
+                CloneSubKey(root, subKey, newName);
+            }
+
+            root.DeleteSubKeyTree(oldName);
+        }
+
+        public static void CloneSubKey(RegistryKey root, RegistryKey source, string newName)
+        {
+            using (RegistryKey target = root.CreateSubKey(newName))
+            {
+
+                foreach (string name in source.GetValueNames())
+                {
+                    target.SetValue(name, source.GetValue(name), source.GetValueKind(name));
+                }
+
+                foreach (string name in source.GetSubKeyNames())
+                {
+                    using (RegistryKey subKey = source.OpenSubKey(name))
+                    {
+                        CloneSubKey(target, subKey, name);
+                    }
+                }
+            }
+
+        }
+
+        [DllImport("advapi32.dll")]
+        public static extern int RegLoadKey(IntPtr hkey, string lpSubKey, string lpFile);
+
+        internal static readonly IntPtr HKEY_USERS = new IntPtr(unchecked((int)0x80000003));
+    }
+}
